b635f002312938b07b417c4503fd1add44169e54
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2022-01-14 22:24:00 +0530

    created array folder

--- Arrays/duplicateElements.java
+++ Arrays/duplicateElements.java
@@ -1,3 +1,14 @@
+/*Write a program to remove consecutive duplicate elements in an array.
+
+Input values
+
+10 10 30 30 40 50 50 50 9 45
+
+Output 
+ 
+10 30 40 50 9 45
+*/
+
 import java.util.Scanner;
 
 public class duplicateElements
