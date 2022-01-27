ebc724356e2678b0326096449c550e60eac1d609
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2022-01-28 00:26:37 +0530

    total cases to reach 4 using number 1, 2, 3

--- src/Recursion/stairCase.java
+++ src/Recursion/stairCase.java
@@ -5,12 +5,30 @@ public class stairCase
     public static int count(int curr, int end)
     {
         if(curr==end)
+        {
+            System.out.println("=="+curr);
+            System.out.println();
             return 1;
+        }
         if(curr>end)
+        {
+            System.out.println(">"+curr);
+            System.out.println();
             return 0;
+        }
         int c=0;
         for(int i=1;i<=3;i++)
-            c+=count(curr+i, end);
+        {
+            System.out.println("i="+i);
+            System.out.println("curr="+curr);
+            System.out.println("c="+c);
+            c += count(curr + i, end);
+            System.out.println();
+            System.out.println("i="+i);
+            System.out.println("curr="+curr);
+            System.out.println("c="+c);
+            System.out.println();
+        }
         return c;
     }
     public static void main(String[] args)
