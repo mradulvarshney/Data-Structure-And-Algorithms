6087b2cfd9d48224c4db12271f2755f9d7cdf643
Mradul Varshney <80679669+mradulvarshney@users.noreply.github.com>
2022-04-02 23:55:12 +0530

    java program

---
+++ src/Recursion/factorial.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class factorial
+{
+    public static int fact(int n)
+    {
+        if(n==0)
+            return 1;
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(fact(6));
+    }
+}
